fd782d64dec6650e017f98988b4f98d6a3225e74
NerminB <nrmnbabalik@gmail.com>
2022-07-12 23:18:01 +0300

    Add project files.

---
+++ MyDictionaryOdev/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionaryOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionaryOdev<string> myDictionaryOdev = new MyDictionaryOdev<string>();
+            myDictionaryOdev.Add("Nermin");
+            myDictionaryOdev.Add("SpiderMan");
+            myDictionaryOdev.Add("Batman");
+            Console.WriteLine(myDictionaryOdev.Count);
+        }
+    }
+}
